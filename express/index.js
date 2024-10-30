ede3aa23ecc9a5f46faaee8be2de24d504e9d755
Rounak Tikadar <rounak.tikadar@gmail.com>
2024-10-30 13:36:49 +0800

    added get requests for todos in express

---
+++ express/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+const jsonplaceholderdomain = 'https://jsonplaceholder.typicode.com'
+const jsonplaceholdertodos = jsonplaceholderdomain + '/todos'
+
+app.use(express.json())
+
+app.post('/todos', (req, res) => {
+    res.sendStatus(501)
+})
+
+app.get('/todos', (req, res) => {
+    fetch(jsonplaceholdertodos)
+        .then(result => result.json())
+        .then(data => res.json(data))
+        .catch(err => res.sendStatus(503))
+})
+
+app.get('/todos/:id', (req, res) => {
+    fetch(`${jsonplaceholdertodos}/${req.params.id}`)
+        .then(result => result.json())
+        .then(data => res.json(data))
+        .catch(err => res.sendStatus(503))
+})
+
+app.put('/todos/:id', (req, res) => {
+    res.sendStatus(501)
+})
+
+app.delete('/todos/:id', (req, res) => {
+    res.sendStatus(501)
+})
+
+app.all('*', (req, res) => {
+    res.sendStatus(404)
+})
+
+app.listen(port, () => {
+    console.log(`Express running on port: ${port}`)
+})
